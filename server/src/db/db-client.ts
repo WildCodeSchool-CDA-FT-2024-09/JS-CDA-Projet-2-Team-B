27425d130d488a5d8e532e5caee766fdd3502cc5
pierre delarocque <delarocque.pierre@gmail.com>
2024-11-22 10:12:34 +0100

    config/setup prettier and eslint

---
+++ server/src/db/db-client.ts
@@ -0,0 +1,18 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+const { POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_USER, POSTGRES_HOST } =
+  process.env;
+
+export const dataSource = new DataSource({
+  type: 'postgres',
+  host: POSTGRES_HOST,
+  port: 5432,
+  username: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+  database: POSTGRES_DB,
+  entities: [],
+  synchronize: true,
+  logging: false
+});
