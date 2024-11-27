b667734992e14efcb373134ff21fa72b86bf2bdc
pierre delarocque <delarocque.pierre@gmail.com>
2024-11-27 09:53:45 +0100

    add .tsx etc in the package.json

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,29 +1,42 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
 
-export default tseslint.config(
-  { ignores: ["dist"] },
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
+    ignores: ['dist'] // Ignorer les fichiers ou dossiers spécifiques
+  },
+  js.configs.recommended, // Configuration recommandée pour JavaScript
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'], // Cible les fichiers pertinents
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parser: tsParser, // Utilisation du parser TypeScript
+      parserOptions: {
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true // Activer le support JSX
+        }
+      }
     },
     plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
+      react, // Plugin React
+      'react-hooks': reactHooks, // Plugin React Hooks
+      'react-refresh': reactRefresh, // Plugin React Refresh
+      '@typescript-eslint': tsPlugin // Plugin TypeScript
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
+      ...reactHooks.configs.recommended.rules, // Règles recommandées pour React Hooks
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true }
       ],
-      "no-console": ["error", { allow: ["error", "info", "warm"] }],
-    },
+      'no-console': ['error', { allow: ['warn', 'error', 'info'] }] // Limiter l'usage de console
+    }
   }
-);
+];
