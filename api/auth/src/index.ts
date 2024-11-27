e0c335e33c5bb4a09ccc720da2addf8b8969bb1e
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-11-27 23:35:32 +0100

    database connexion setup on services

--- api/auth/src/index.ts
+++ api/auth/src/index.ts
@@ -1,6 +1,7 @@
 import express, { json } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import { pool } from './database/pg.client';
 
 const app = express();
 
@@ -15,6 +16,16 @@ app.use(cors(corsOptions));
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.info(`Listening on port ${PORT}`);
+
+  try {
+    const client = await pool.connect();
+    console.info('✅ Connected to the database.');
+    client.release();
+  } catch (error) {
+    console.error('❌ Failed to connect to the database:', error);
+  } finally {
+    await pool.end();
+  }
 });
