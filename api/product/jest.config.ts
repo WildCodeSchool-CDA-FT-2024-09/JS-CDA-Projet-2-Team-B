155b31c8249de67269ce0396361796a16fd189f8
JohaneDecamps <johane.decamps@gmail.com>
2024-12-02 11:14:35 +0100

    remove rootDir

--- api/product/jest.config.ts
+++ api/product/jest.config.ts
@@ -1,12 +1,11 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-  rootDir: './api/product',
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['<rootDir>/tests/**/*.test.ts'],
+  testMatch: ['./src/**/*.test.ts'],
   moduleFileExtensions: ['ts', 'js'],
-  setupFilesAfterEnv: ['<rootDir>/tests/jest.setup.ts']
+  setupFilesAfterEnv: ['./tests/jest.setup.ts']
 };
 
 export default config;
