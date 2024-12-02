74eabed5b78b4d1e773e89509f71e9b6b5be1410
pierre delarocque <delarocque.pierre@gmail.com>
2024-12-02 12:08:58 +0100

    add test for each service

--- api/email/jest.config.ts
+++ api/email/jest.config.ts
@@ -3,9 +3,9 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['<rootDir>/src/**/*.test.ts'],
+  testMatch: ['./src/**/*.test.ts'],
   moduleFileExtensions: ['ts', 'js'],
-  setupFilesAfterEnv: ['<rootDir>/tests/jest.setup.ts']
+  setupFilesAfterEnv: ['./tests/jest.setup.ts']
 };
 
 export default config;
