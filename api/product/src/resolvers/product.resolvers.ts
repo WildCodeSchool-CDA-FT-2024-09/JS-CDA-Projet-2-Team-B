b276b1803615646bd4b7a6d91e67323fbd27a1ea
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-12-02 22:53:27 +0100

    feat: update product mutation

--- api/product/src/resolvers/product.resolvers.ts
+++ api/product/src/resolvers/product.resolvers.ts
@@ -1,10 +1,41 @@
+import { Resolver, Query, Mutation, Arg } from 'type-graphql';
+import { ProductUpdateInput } from '../types/product.types';
 import { Product } from '../entity/product.entities';
-import { Resolver, Query } from 'type-graphql';
 
 @Resolver(Product)
 export default class ProductResolver {
   @Query(() => [Product])
   async getAllProducts() {
     return [];
+  }
+
+  @Mutation(() => Product)
+  async updateProduct(
+    @Arg('data') newDataProduct: ProductUpdateInput
+  ): Promise<Product> {
+    const { id } = newDataProduct;
+
+    const product = await Product.findOne({
+      where: { id }
+    });
+
+    if (!product) {
+      throw new Error('Product not found');
+    }
+
+    // Using assign method from Object to assign new data to the found product.
+    Object.assign(product, newDataProduct);
+
+    await product.save();
+
+    const updatedProduct = await Product.findOne({
+      where: { id }
+    });
+
+    if (!updatedProduct) {
+      throw new Error('Product not found');
+    }
+
+    return updatedProduct;
   }
 }
