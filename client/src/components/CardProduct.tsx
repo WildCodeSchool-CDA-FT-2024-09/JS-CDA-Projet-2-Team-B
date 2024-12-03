aa9aa5e3505d9026e2aab40f5e4dc1615c7642b3
JohaneDecamps <johane.decamps@gmail.com>
2024-12-03 17:03:08 +0100

    card product

--- client/src/components/CardProduct.tsx
+++ client/src/components/CardProduct.tsx
@@ -1,17 +1,59 @@
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import { Grid } from '@mui/system';
 
-export default function CardProduct() {
+const styleButton = {
+  padding: '4px 10px',
+  borderRadius: '5px',
+  margin: 0.6,
+  color: 'primary.contrastText',
+  textTransform: 'uppercase',
+  textDecoration: 'none',
+  fontFamily: "'Roboto', sans-serif"
+};
+
+type Products = {
+  nom: string;
+  reference: string;
+  categorie: string;
+  description: string;
+};
+
+export default function CardProduct({
+  nom,
+  reference,
+  categorie,
+  description
+}: Products) {
   return (
-    <Card sx={{ maxWidth: 250 }}>
-      <p> Hello </p>
+    <Card
+      sx={{
+        maxWidth: 300,
+        margin: 1,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        boxShadow: 4
+      }}
+    >
       <CardContent>
-        <Typography variant="body2"> Nom du produit</Typography>
-        <Typography variant="body2"> Ref du produit</Typography>
-        <Typography variant="body2"> Categorie du produit</Typography>
-        <Typography variant="body2"> Description du produit</Typography>
+        <Typography variant="h6" sx={{ marginBottom: 1 }}>
+          {nom}
+        </Typography>
+        <Typography variant="body2"> {reference}</Typography>
+        <Typography variant="body2"> {categorie}</Typography>
+        <Typography variant="body2"> {description}</Typography>
       </CardContent>
+      <Grid sx={{ display: 'flex', justifyContent: 'end', marginRight: 1 }}>
+        <Link href="/" sx={{ ...styleButton, backgroundColor: 'green' }}>
+          Modifier
+        </Link>
+        <Link href="/" sx={{ ...styleButton, backgroundColor: 'info.main' }}>
+          Activer
+        </Link>
+      </Grid>
     </Card>
   );
 }
--- client/src/pages/Catalog.tsx
+++ client/src/pages/Catalog.tsx
@@ -1,10 +1,57 @@
 import CardProduct from '../components/CardProduct';
+import Grid from '@mui/material/Grid2';
+
+const produits = [
+  {
+    nom: 'Vélo de Course',
+    reference: 'VC-1234',
+    categorie: 'Sports',
+    description:
+      'Un vélo léger et rapide, parfait pour les courses sur route et les longues distances.'
+  },
+  {
+    nom: 'Smartphone 5G',
+    reference: 'SP-5678',
+    categorie: 'Electronique',
+    description:
+      'Un smartphone dernier cri avec écran OLED et caméra ultra haute définition.'
+  },
+  {
+    nom: 'Chaise Ergonomique',
+    reference: 'CE-9101',
+    categorie: 'Mobilier',
+    description:
+      'Une chaise ergonomique conçue pour un confort optimal lors de longues heures de travail.'
+  },
+  {
+    nom: 'Montre Connectée',
+    reference: 'MC-1122',
+    categorie: 'Accessoires',
+    description:
+      "Une montre connectée avec suivi d'activité et notifications en temps réel."
+  }
+];
 
 export default function Catalog() {
   return (
-    <div>
-      <CardProduct />
-      <p> Bonjour</p>
-    </div>
+    <Grid
+      container
+      spacing={2}
+      sx={{
+        marginTop: 6,
+        marginRight: 10,
+        justifyContent: 'right'
+      }}
+    >
+      {produits.map((produit) => (
+        <CardProduct
+          key={produit.reference}
+          nom={produit.nom}
+          reference={produit.reference}
+          categorie={produit.categorie}
+          description={produit.description}
+        />
+      ))}
+    </Grid>
   );
 }
