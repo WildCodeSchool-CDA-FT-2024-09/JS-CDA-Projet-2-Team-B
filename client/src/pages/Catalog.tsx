ee974ac89db7b7806fc31a63d453aca0a21171b7
pierre delarocque <delarocque.pierre@gmail.com>
2024-12-03 14:35:36 +0100

    fix materiel ui witch docker

---
+++ client/src/pages/Catalog.tsx
@@ -0,0 +1,14 @@
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+export default function Catalog() {
+  return (
+    <div>
+      <Stack spacing={2} direction="row">
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+    </div>
+  );
+}
