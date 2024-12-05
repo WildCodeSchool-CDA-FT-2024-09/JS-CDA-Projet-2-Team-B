44a85312ffe34c631c6ceb39893cf3d1e5855852
alex <alexandre.ag.67@gmail.com>
2024-12-05 15:28:18 +0100

    implement soft delete and restore operations

--- api/product/src/resolvers/category.resolver.ts
+++ api/product/src/resolvers/category.resolver.ts
@@ -1,5 +1,13 @@
 import { Length } from 'class-validator';
-import { Arg, Field, InputType, Mutation, Query, Resolver } from 'type-graphql';
+import {
+  Arg,
+  Field,
+  InputType,
+  Int,
+  Mutation,
+  Query,
+  Resolver
+} from 'type-graphql';
 import { Category } from '../entity/category.entities';
 
 @InputType()
@@ -22,8 +30,16 @@ export class UpdateCategoryInput {
 @Resolver()
 export default class CategoryResolver {
   @Query(() => [Category])
-  async getAllCategories(): Promise<Category[]> {
-    return await Category.find();
+  async getAllCategories(
+    @Arg('includeDeleted', () => Boolean, { nullable: true })
+    includeDeleted = false
+  ): Promise<Category[]> {
+    return await Category.find({
+      withDeleted: includeDeleted,
+      order: {
+        name: 'ASC'
+      }
+    });
   }
 
   @Mutation(() => Category)
@@ -78,6 +94,46 @@ export default class CategoryResolver {
       throw new Error(
         `Erreur lors de la mise à jour de la catégorie: ${error.message}`
       );
+    }
+  }
+
+  @Mutation(() => Boolean)
+  async deleteCategory(@Arg('id') id: number): Promise<boolean> {
+    try {
+      const category = await Category.findOne({
+        where: { id }
+      });
+
+      if (!category) {
+        throw new Error('Category not found');
+      }
+
+      await category.softRemove();
+
+      return true;
+    } catch (error) {
+      console.error('Error while deleting category:', error);
+      throw error;
+    }
+  }
+
+  @Mutation(() => Boolean)
+  async restoreCategory(@Arg('id', () => Int) id: number): Promise<boolean> {
+    try {
+      const category = await Category.findOne({
+        where: { id },
+        withDeleted: true
+      });
+
+      if (!category) {
+        throw new Error('Category not found');
+      }
+
+      await category.recover();
+      return true;
+    } catch (error) {
+      console.error('Error restoring category:', error);
+      throw error;
     }
   }
 }
