db3301af5cf98e85c45655fe629485c614924587
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-12-12 15:01:44 +0100

    BrandManagement page creation

--- client/src/pages/BrandManagement.tsx
+++ client/src/pages/BrandManagement.tsx
@@ -24,16 +24,33 @@ export default function BrandManagement() {
         >
           <Box
             sx={{
-              backgroundColor: '#E3AB44',
-              borderRadius: '2px',
-              padding: '5px',
               display: 'flex',
-              justifyContent: 'center'
+              flexDirection: 'column',
+              gap: '1.5dvh'
             }}
           >
             <Link
+              to="/brand/view"
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                borderRadius: '2px',
+                padding: '5px',
+                backgroundColor: '#E3AB44',
+                textDecoration: 'none',
+                color: 'inherit'
+              }}
+            >
+              Liste des marques
+            </Link>
+            <Link
               to="/brand/add"
               style={{
+                display: 'flex',
+                justifyContent: 'center',
+                borderRadius: '2px',
+                padding: '5px',
+                backgroundColor: '#E3AB44',
                 textDecoration: 'none',
                 color: 'inherit'
               }}
@@ -43,7 +60,24 @@ export default function BrandManagement() {
           </Box>
         </CardContent>
       </Card>
-      <Outlet />
+      <Card
+        sx={{
+          width: '85%',
+          margin: '5px 0',
+          height: '100dvh',
+          boxShadow: '5px 5px 15px rgba(0, 0, 0, 0.2)',
+          display: 'flex',
+          justifyContent: 'left'
+        }}
+      >
+        <CardContent
+          sx={{
+            marginLeft: '5dvh'
+          }}
+        >
+          <Outlet />
+        </CardContent>
+      </Card>
     </Box>
   );
 }
