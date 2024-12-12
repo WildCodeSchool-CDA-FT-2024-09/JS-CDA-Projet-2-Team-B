607f23935b3d7380348438d7a8e1e84b9411218f
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-12-12 14:48:43 +0100

    Created brand creation function

--- api/product/src/__test__/helpers/test.helpers.ts
+++ api/product/src/__test__/helpers/test.helpers.ts
@@ -1,23 +1,23 @@
-// import { graphql, GraphQLSchema, print } from 'graphql';
-// import getSchema from '../../schema';
-// import { Brand } from '../../entity/brand.entities';
-// import { generateRandomString } from './generateRandomString.helper';
-// import { POST_BRAND } from '../schemas/mutations';
+import { graphql, GraphQLSchema, print } from 'graphql';
+import getSchema from '../../schema';
+import { Brand } from '../../entity/brand.entities';
+import { generateRandomString } from './generateRandomString.helper';
+import { POST_BRAND } from '../schemas/mutations';
 
-// export const createBrand = async (): Promise<Brand> => {
-//   const schema: GraphQLSchema = await getSchema();
+export const createBrand = async (): Promise<Brand> => {
+  const schema: GraphQLSchema = await getSchema();
 
-//   const newBrandData = {
-//     name: generateRandomString(10),
-//     description: generateRandomString(10),
-//     logo: generateRandomString(10)
-//   };
+  const newBrandData = {
+    name: generateRandomString(10),
+    description: generateRandomString(10),
+    logo: generateRandomString(10)
+  };
 
-//   const result = (await graphql({
-//     schema: schema,
-//     source: print(POST_BRAND),
-//     variableValues: { data: newBrandData }
-//   })) as { data: { createBrand: Brand } };
+  const result = (await graphql({
+    schema: schema,
+    source: print(POST_BRAND),
+    variableValues: { data: newBrandData }
+  })) as { data: { createBrand: Brand } };
 
-//   return result.data.createBrand;
-// };
+  return result.data.createBrand;
+};
