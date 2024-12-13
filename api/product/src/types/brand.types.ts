08dbe3040d985e38aa050aa3f13733238a74787e
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-12-13 12:17:51 +0100

    Update brand input

--- api/product/src/types/brand.types.ts
+++ api/product/src/types/brand.types.ts
@@ -12,3 +12,18 @@ export class BrandCreationInput implements Partial<Brand> {
   @Field()
   logo: string;
 }
+
+@InputType()
+export class BrandUpdateInput implements Partial<Brand> {
+  @Field()
+  id: number;
+
+  @Field({ nullable: true })
+  name: string;
+
+  @Field({ nullable: true })
+  description: string;
+
+  @Field({ nullable: true })
+  logo: string;
+}
