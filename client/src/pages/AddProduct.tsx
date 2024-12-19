3b077526c8c5c08bc27f7782bdf21b19b4ee54a8
pierre delarocque <delarocque.pierre@gmail.com>
2024-12-19 15:50:45 +0100

    Front/AddProduct modification css for the 2 componants

--- client/src/pages/AddProduct.tsx
+++ client/src/pages/AddProduct.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import CreationProduct from '../components/CreationProduct';
 import AddImage from '../components/AddImage';
+import { Grid, Grid2 } from '@mui/material';
 
 const AddProduct = () => {
   const [productId, setProductId] = useState<number | null>(null);
@@ -12,10 +13,16 @@ const AddProduct = () => {
   };
 
   return (
-    <>
-      <CreationProduct handleProductId={handleProductId} block={block} />
-      {productId && <AddImage productId={productId} handleBlock={setBlock} />}
-    </>
+    <Grid2 container spacing={2}>
+      <Grid item xs={12}>
+        <CreationProduct handleProductId={handleProductId} block={block} />
+      </Grid>
+      {productId && (
+        <Grid item xs={12} md={6}>
+          <AddImage productId={productId} handleBlock={setBlock} />
+        </Grid>
+      )}
+    </Grid2>
   );
 };
 
