531a7da996e7d590d09090ff2d0a0f2741b21b23
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-01-03 10:01:15 +0100

    Added a method to handle brand image creation

--- api/uploadimage/src/services/upload.ts
+++ api/uploadimage/src/services/upload.ts
@@ -45,3 +45,27 @@ export const imageByProduct = async (imageId: number, productId: string) => {
     throw error;
   }
 };
+
+type ImageProps = {
+  id: number;
+  url: string;
+  isMain: boolean;
+};
+
+export const updateBrandImage = async (
+  imageId: ImageProps,
+  brandId: string
+) => {
+  const query = 'UPDATE brand SET "image_id" = $1 WHERE "id" = $2';
+
+  try {
+    await pool.query(query, [imageId, brandId]);
+    console.info("L'image a correctement été associée à la marque.");
+  } catch (error) {
+    console.error(
+      "Erreur lors de l'association de l'image à la marque : ",
+      error
+    );
+    throw error;
+  }
+};
