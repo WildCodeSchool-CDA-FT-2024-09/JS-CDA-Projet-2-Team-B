17e4f4f38ea363143024dc42100d3178017bd4db
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-01-03 10:16:04 +0100

    Added image selection component after brand's creation

--- client/src/components/AddBrand.tsx
+++ client/src/components/AddBrand.tsx
@@ -1,6 +1,7 @@
 import { Box, Button, TextField, Typography } from '@mui/material';
 import React, { useState } from 'react';
 import { useCreateBrandMutation } from '../generated/graphql-types';
+import AddBrandImage from './AddBrandImage';
 
 interface BrandReq {
   name: string;
@@ -16,6 +17,8 @@ export default function AddBrand() {
   });
   const [createBrand, { loading, error }] = useCreateBrandMutation();
   const [successMessage, setSuccessMessage] = useState<string | null>(null);
+  const [imageSelection, setImageSelection] = useState(false);
+  const [brandId, setBrandId] = useState<number | null>(null);
 
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -52,6 +55,8 @@ export default function AddBrand() {
           logo: ''
         });
         setSuccessMessage('Marque créée avec succès !');
+        setImageSelection(true);
+        setBrandId(data.createBrand.id);
       }
     } catch (err) {
       setSuccessMessage(null);
@@ -60,90 +65,93 @@ export default function AddBrand() {
   };
 
   return (
-    <Box
-      component="form"
-      onSubmit={handleSubmit}
-      sx={{
-        m: 1,
-        width: '60ch',
-        fontWeight: 'bold',
-        display: 'flex',
-        flexDirection: 'column',
-        gap: 1,
-        maxWidth: 400,
-        margin: '0 auto'
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <Typography
+    <>
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
         sx={{
-          marginLeft: '2px',
-          fontWeight: 'bold'
+          m: 1,
+          width: '60ch',
+          fontWeight: 'bold',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 1,
+          maxWidth: 400,
+          margin: '0 auto'
         }}
+        noValidate
+        autoComplete="off"
       >
-        Nom
-      </Typography>
-      <TextField
-        required
-        id="outlined-required"
-        name="name"
-        value={brand.name}
-        onChange={handleChange}
-        placeholder="Nom"
-      />
-      <Typography
-        sx={{
-          marginLeft: '2px',
-          fontWeight: 'bold'
-        }}
-      >
-        Description
-      </Typography>
-      <TextField
-        required
-        id="outlined-required"
-        name="description"
-        value={brand.description}
-        onChange={handleChange}
-        placeholder="Description"
-      />
-      <Button
-        variant="contained"
-        disabled={loading}
-        color="primary"
-        type="submit"
-        sx={{
-          width: '20ch',
-          alignSelf: 'flex-end'
-        }}
-      >
-        Enregistrer
-      </Button>
-      {successMessage && (
         <Typography
-          color="success.main"
-          variant="body2"
           sx={{
-            display: 'flex',
-            justifyContent: 'end'
+            marginLeft: '2px',
+            fontWeight: 'bold'
           }}
         >
-          {successMessage}
+          Nom
         </Typography>
-      )}
-      {error && (
+        <TextField
+          required
+          id="outlined-required"
+          name="name"
+          value={brand.name}
+          onChange={handleChange}
+          placeholder="Nom"
+        />
         <Typography
-          color="error.main"
-          variant="body2"
           sx={{
-            display: 'flex',
-            justifyContent: 'end'
+            marginLeft: '2px',
+            fontWeight: 'bold'
           }}
         >
-          Une erreur s'est produite : {error.message}
+          Description
         </Typography>
-      )}
-    </Box>
+        <TextField
+          required
+          id="outlined-required"
+          name="description"
+          value={brand.description}
+          onChange={handleChange}
+          placeholder="Description"
+        />
+        <Button
+          variant="contained"
+          disabled={loading}
+          color="primary"
+          type="submit"
+          sx={{
+            width: '20ch',
+            alignSelf: 'flex-end'
+          }}
+        >
+          Enregistrer
+        </Button>
+        {successMessage && (
+          <Typography
+            color="success.main"
+            variant="body2"
+            sx={{
+              display: 'flex',
+              justifyContent: 'end'
+            }}
+          >
+            {successMessage}
+          </Typography>
+        )}
+        {error && (
+          <Typography
+            color="error.main"
+            variant="body2"
+            sx={{
+              display: 'flex',
+              justifyContent: 'end'
+            }}
+          >
+            Une erreur s'est produite : {error.message}
+          </Typography>
+        )}
+      </Box>
+      {imageSelection && <AddBrandImage brandId={brandId} />}
+    </>
   );
 }
