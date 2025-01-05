15f47749f79d9986cb55a880e4ffcb6cab5266ee
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-01-05 19:08:55 +0100

    Patch route deletes previous image + delete route impl

--- api/uploadimage/src/routes/brand.routes.ts
+++ api/uploadimage/src/routes/brand.routes.ts
@@ -1,40 +1,80 @@
 import { Router, Request, Response } from 'express';
 import {
+  checkIfBrandHasImage,
+  deleteBrandImageFile,
+  deleteBrandImageFromDatabase,
+  getBrandByImageId,
+  getBrandImage,
   saveImageToDatabase,
   updateBrandImage,
+  updateBrandImageToNull,
   verifyFileExistence
 } from '../services/upload';
 import multer from '../middlewares/multer';
 
 const brandRouter = Router();
 
-brandRouter.patch(
-  '/',
-  multer.single('image'),
-  async (req: Request, res: Response): Promise<void> => {
-    console.info('Headers:', req.headers);
-    console.info('Body:', req.body);
-    console.info('Received file:', req.file);
+brandRouter
+  .patch(
+    '/',
+    multer.single('image'),
+    async (req: Request, res: Response): Promise<void> => {
+      console.info('Headers:', req.headers);
+      console.info('Body:', req.body);
+      console.info('Received file:', req.file);
 
-    const { brand_id } = req.body;
+      const { brand_id } = req.body;
 
-    const isMain = true;
+      const isMain = true;
 
-    if (!req.file) {
-      res.status(400).json({ error: 'No file uploaded' });
-      return;
+      if (!req.file) {
+        res.status(400).json({ error: 'No file uploaded' });
+        return;
+      }
+
+      const filePath = `/${req.file.filename}`;
+
+      try {
+        const brandImage = await checkIfBrandHasImage(brand_id);
+
+        if (brandImage.image_id) {
+          const image = await getBrandImage(brandImage.image_id);
+          await updateBrandImageToNull(brand_id);
+          await deleteBrandImageFromDatabase(brandImage.image_id);
+          await deleteBrandImageFile(image.url);
+        }
+
+        await verifyFileExistence(req.file.path);
+        const savedImage = await saveImageToDatabase(filePath, isMain);
+        await updateBrandImage(savedImage.id, brand_id);
+
+        res.status(200).json({
+          status: 'success',
+          data: savedImage
+        });
+        return;
+      } catch (error) {
+        console.error('Error:', error instanceof Error ? error.message : error);
+        res.sendStatus(500);
+        return;
+      }
     }
-
-    const filePath = `/${req.file.filename}`;
+  )
+  .delete('/', async (req: Request, res: Response): Promise<void> => {
+    const { image_id } = req.body;
 
     try {
-      await verifyFileExistence(req.file.path);
-      const savedImage = await saveImageToDatabase(filePath, isMain);
-      await updateBrandImage(savedImage.id, brand_id);
+      const brand = await getBrandByImageId(image_id);
+
+      if (brand) {
+        const image = await getBrandImage(brand.image_id);
+        await updateBrandImageToNull(brand.id);
+        await deleteBrandImageFromDatabase(brand.image_id);
+        await deleteBrandImageFile(image.url);
+      }
 
       res.status(200).json({
-        status: 'success',
-        data: savedImage
+        status: 'success'
       });
       return;
     } catch (error) {
@@ -42,7 +82,6 @@ brandRouter.patch(
       res.sendStatus(500);
       return;
     }
-  }
-);
+  });
 
 export default brandRouter;
