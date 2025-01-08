80eca132e684b962e4c20b2b41558855d00df740
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-01-08 10:19:39 +0100

    Custom Error creation + Bad Request & DB Connection Error impl

---
+++ api/auth/src/errors/index.errors.ts
@@ -0,0 +1,5 @@
+import { BadRequestError } from './BadRequestError.error';
+import { CustomError } from './CustomError.error';
+import { DatabaseConnectionError } from './DatabaseConnectionError.error';
+
+export { BadRequestError, CustomError, DatabaseConnectionError };
