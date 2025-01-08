fc3778ec3ad6088d9ccfcaead125083d7d7dd61a
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-01-08 10:44:21 +0100

    User datamapper impl

---
+++ api/auth/src/datamappers/UserDatamapper.ts
@@ -0,0 +1,9 @@
+import { TableNames } from 'helpers/TableNames';
+import { CoreDatamapper } from './CoreDatamapper';
+import { UserDatamapperReq } from './interfaces/UserDatamapperReq';
+import { pool } from 'database/pg.client';
+
+export class UserDatamapper extends CoreDatamapper<UserDatamapperReq> {
+  readonly tableName = TableNames.User;
+  pool = pool;
+}
--- api/auth/src/datamappers/index.datamappers.ts
+++ api/auth/src/datamappers/index.datamappers.ts
@@ -1,5 +1,7 @@
 import { RoleDatamapper } from './RoleDatamapper';
+import { UserDatamapper } from './UserDatamapper';
 
 const roleDatamapper = new RoleDatamapper();
+const userDatamapper = new UserDatamapper();
 
-export { roleDatamapper };
+export { roleDatamapper, userDatamapper };
