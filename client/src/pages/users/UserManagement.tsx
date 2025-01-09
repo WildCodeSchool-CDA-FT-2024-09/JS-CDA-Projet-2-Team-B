369f84bea41ef7fcaf5f4db7ebcd6714d025154f
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-01-09 22:34:27 +0100

    refactor: moved data grid columns definition into its own file

--- client/src/pages/users/UserManagement.tsx
+++ client/src/pages/users/UserManagement.tsx
@@ -1,8 +1,9 @@
 import Box from '@mui/material/Box';
-import { DataGrid, GridActionsCellItem, GridColDef } from '@mui/x-data-grid';
+import { DataGrid } from '@mui/x-data-grid';
 import { useState } from 'react';
 import { Button } from '@mui/material';
-import { Add, ControlPoint } from '@mui/icons-material';
+import { Add } from '@mui/icons-material';
+import { userColumns } from '../../utils/userColumns';
 import axios from 'axios';
 
 export type UserRow = {
@@ -68,89 +69,6 @@ const rows: UserRow[] = [
 export default function UserManagement() {
   const [users, setUsers] = useState<UserRow[]>(rows);
 
-  const columns: GridColDef<UserRow>[] = [
-    { field: 'id', headerName: 'ID', width: 40 },
-    {
-      field: 'status',
-      headerName: 'Status',
-      width: 110,
-      valueGetter: (value, row) => {
-        if (!row.starting_date || !row.ending_date) {
-          return '';
-        }
-        const today = new Date();
-        const startDate = new Date(row.starting_date);
-        const endDate = new Date(row.ending_date);
-        return startDate <= today && today <= endDate ? 'Actif' : 'Inactif';
-      }
-    },
-    {
-      field: 'first_name',
-      headerName: 'Prénom',
-      width: 150,
-      editable: true
-    },
-    {
-      field: 'last_name',
-      headerName: 'Nom',
-      width: 150,
-      editable: true
-    },
-    {
-      field: 'full_name',
-      headerName: 'Nom complet',
-      description:
-        'Cette colonne est générée par les colonnes "Prénom" et "Nom" et ne peut pas être triée.',
-      sortable: false,
-      width: 160,
-      valueGetter: (value, row) =>
-        `${row.first_name || ''} ${row.last_name || ''}`
-    },
-    {
-      field: 'email',
-      headerName: 'Email',
-      width: 250,
-      editable: true
-    },
-    {
-      field: 'phone',
-      headerName: 'nº téléphone',
-      width: 110,
-      editable: true
-    },
-    {
-      field: 'starting_date',
-      headerName: 'Date de début',
-      width: 110,
-      editable: true
-    },
-    {
-      field: 'ending_date',
-      headerName: 'Date de fin',
-      width: 110,
-      editable: true
-    },
-    {
-      field: 'actions',
-      type: 'actions',
-      headerName: 'Actions',
-      width: 100,
-      cellClassName: 'actions',
-      getActions: (e) => {
-        if (e.row.isNew) {
-          return [
-            <GridActionsCellItem
-              icon={<ControlPoint />}
-              label="Save"
-              onClick={() => handleSaveClick(e.row)}
-            />
-          ];
-        }
-        return [];
-      }
-    }
-  ];
-
   const handleSaveClick = async (e: UserRow) => {
     const { id, isNew, ...dataCleaned } = e;
 
@@ -194,7 +112,7 @@ export default function UserManagement() {
     <Box sx={{ height: 'auto', width: 'auto', margin: '2%' }}>
       <DataGrid
         rows={users}
-        columns={columns}
+        columns={userColumns(handleSaveClick)}
         initialState={{
           pagination: {
             paginationModel: {
---
+++ client/src/utils/userColumns.tsx
@@ -0,0 +1,92 @@
+import { GridActionsCellItem, GridColDef } from '@mui/x-data-grid';
+import { ControlPoint } from '@mui/icons-material';
+import { UserRow } from '../pages/users/UserManagement';
+
+interface ColumnsProps {
+  handleSaveClick: (e: UserRow) => void;
+}
+
+export const userColumns = (
+  handleSaveClick: ColumnsProps['handleSaveClick']
+): GridColDef<UserRow>[] => [
+  { field: 'id', headerName: 'ID', width: 40 },
+  {
+    field: 'status',
+    headerName: 'Status',
+    width: 110,
+    valueGetter: (value, row) => {
+      if (!row.starting_date || !row.ending_date) {
+        return '';
+      }
+      const today = new Date();
+      const startDate = new Date(row.starting_date);
+      const endDate = new Date(row.ending_date);
+      return startDate <= today && today <= endDate ? 'Actif' : 'Inactif';
+    }
+  },
+  {
+    field: 'first_name',
+    headerName: 'Prénom',
+    width: 150,
+    editable: true
+  },
+  {
+    field: 'last_name',
+    headerName: 'Nom',
+    width: 150,
+    editable: true
+  },
+  {
+    field: 'full_name',
+    headerName: 'Nom complet',
+    description:
+      'Cette colonne est générée par les colonnes "Prénom" et "Nom" et ne peut pas être triée.',
+    sortable: false,
+    width: 160,
+    valueGetter: (value, row) =>
+      `${row.first_name || ''} ${row.last_name || ''}`
+  },
+  {
+    field: 'email',
+    headerName: 'Email',
+    width: 250,
+    editable: true
+  },
+  {
+    field: 'phone',
+    headerName: 'nº téléphone',
+    width: 110,
+    editable: true
+  },
+  {
+    field: 'starting_date',
+    headerName: 'Date de début',
+    width: 110,
+    editable: true
+  },
+  {
+    field: 'ending_date',
+    headerName: 'Date de fin',
+    width: 110,
+    editable: true
+  },
+  {
+    field: 'actions',
+    type: 'actions',
+    headerName: 'Actions',
+    width: 100,
+    cellClassName: 'actions',
+    getActions: (e) => {
+      if (e.row.isNew) {
+        return [
+          <GridActionsCellItem
+            icon={<ControlPoint />}
+            label="Save"
+            onClick={() => handleSaveClick(e.row)} // Utilisation de la fonction passée en paramètre
+          />
+        ];
+      }
+      return [];
+    }
+  }
+];
