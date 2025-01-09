7212b8d76e5e48c8d40ed0e2c3a248ce3cc2cbc0
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-01-09 20:28:06 +0100

    Moved insert method into User datamapper

--- api/auth/src/datamappers/CoreDatamapper.ts
+++ api/auth/src/datamappers/CoreDatamapper.ts
@@ -19,15 +19,4 @@ export abstract class CoreDatamapper<T extends EntityDatamapperReq> {
     );
     return result.rows[0];
   };
-
-  insert = async (entityObject: T['data']) => {
-    const columns = Object.keys(entityObject).join(', ');
-    const values = Object.values(entityObject);
-    const placeholders = values.map((_, index) => `$${index + 1}`).join(', ');
-
-    const query = `INSERT INTO "${this.tableName}" (${columns}) VALUES (${placeholders}) RETURNING *`;
-    const result = await this.pool.query(query, values);
-
-    return result.rows[0];
-  };
 }
--- api/auth/src/datamappers/UserDatamapper.ts
+++ api/auth/src/datamappers/UserDatamapper.ts
@@ -6,4 +6,32 @@ import { pool } from '../database/pg.client';
 export class UserDatamapper extends CoreDatamapper<UserDatamapperReq> {
   readonly tableName = TableNames.User;
   pool = pool;
+
+  insert = async (data: UserDatamapperReq['data']) => {
+    const {
+      first_name,
+      last_name,
+      email,
+      password,
+      phone,
+      starting_date,
+      ending_date,
+      role_id
+    } = data;
+
+    const query = `INSERT INTO "${this.tableName}" ("first_name", "last_name", "email", "password", "phone", "starting_date", "ending_date", "role_id") VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`;
+
+    const result = await this.pool.query(query, [
+      first_name,
+      last_name,
+      email,
+      password,
+      phone,
+      starting_date,
+      ending_date,
+      role_id
+    ]);
+
+    return result.rows[0];
+  };
 }
