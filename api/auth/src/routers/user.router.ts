4ebbe261ea374fa840f2cab9ee0941ca203d4cf5
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-01-09 10:41:02 +0100

    create user route impl

--- api/auth/src/routers/user.router.ts
+++ api/auth/src/routers/user.router.ts
@@ -1,5 +1,9 @@
 import Router from 'express';
+import { userController } from '../controllers/index.controllers';
+import { errorCatcher } from '../helpers/errorCatcher.helper';
 
 const userRouter = Router();
+
+userRouter.route('/').post(errorCatcher(userController.create));
 
 export default userRouter;
