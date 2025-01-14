3c17ec5a62ca5d5c0c01417f15e376fbbe498708
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-01-14 22:25:35 +0100

    Error catcher and handler, token check middlewares impl

---
+++ api/uploadimage/src/middlewares/index.middlewares.ts
@@ -0,0 +1,5 @@
+import { errorCatcher } from './errorCatcher.middleware';
+import { errorHandler } from './errorHandler.middleware';
+import { requireAuth } from './requireAuth.middleware';
+
+export { errorCatcher, errorHandler, requireAuth };
