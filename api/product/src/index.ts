19002507aa6415bd50d19df3e6d9abeda3b070bd
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-01-14 22:19:28 +0100

    Context added to handle cookie and verifying jwt

--- api/product/src/index.ts
+++ api/product/src/index.ts
@@ -3,18 +3,60 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { AppDataSource } from './data-source';
 import getSchema from './schema';
+import jwt from 'jsonwebtoken';
+import { AuthenticationError } from 'type-graphql';
+import { parse } from 'cookie';
 
-const { PORT } = process.env;
+const { PORT, ACCESS_TOKEN_SECRET } = process.env;
+
+interface User {
+  id: number;
+  email: string;
+  role: string;
+}
+
+interface Context {
+  user: User | null;
+}
 
 (async () => {
   await AppDataSource.initialize();
 
   const schema = await getSchema();
 
-  const server = new ApolloServer({ schema });
+  const server = new ApolloServer<Context>({ schema });
 
   const { url } = await startStandaloneServer(server, {
-    listen: { port: Number(PORT) }
+    listen: { port: Number(PORT) },
+    context: async ({ req }: { req: import('http').IncomingMessage }) => {
+      const cookies = req.headers.cookie ? parse(req.headers.cookie) : {};
+      const accessToken = cookies.access_token;
+
+      if (!accessToken) {
+        throw new AuthenticationError("Le token d'accès est manquant.");
+      }
+
+      let user: User | null = null;
+
+      if (accessToken) {
+        try {
+          user = jwt.verify(accessToken, ACCESS_TOKEN_SECRET as string) as User;
+
+          if (user.role !== 'user') {
+            throw new AuthenticationError(
+              "Vous n'êtes pas autorisé à accéder à cette ressource."
+            );
+          }
+        } catch (err) {
+          console.error('JWT invalide : ', err);
+          throw new AuthenticationError(
+            "Token d'accès invalide ou expiré. Veuillez vous reconnecter."
+          );
+        }
+      }
+
+      return { user };
+    }
   });
 
   console.info(`🚀  Server ready at: ${url}`);
