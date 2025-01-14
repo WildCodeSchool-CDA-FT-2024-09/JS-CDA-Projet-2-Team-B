03da11d9e06113b4d23125145517f27a088e7066
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-01-14 22:24:28 +0100

    Token & UserPayload helpers impl

---
+++ api/uploadimage/src/helpers/Token.ts
@@ -0,0 +1,45 @@
+import { BadRequestError } from '../errors/BadRequestError.error';
+import { UserPayload } from './index.helpers';
+import { Response } from 'express';
+import jwt from 'jsonwebtoken';
+
+const { ACCESS_TOKEN_SECRET } = process.env;
+
+export default class Token {
+  static generateAccessToken = async ({ id, email, role }: UserPayload) => {
+    const user = { id, email, role };
+
+    if (!ACCESS_TOKEN_SECRET) {
+      throw new BadRequestError("La clé secrète n'est pas définie.");
+    }
+
+    const accessToken = jwt.sign(user, ACCESS_TOKEN_SECRET, {
+      expiresIn: '30sec'
+    });
+
+    return accessToken;
+  };
+
+  static verify = async (
+    token: string,
+    secret: string
+  ): Promise<UserPayload> => {
+    return new Promise((resolve, reject) => {
+      jwt.verify(token, secret, (err, decoded) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(decoded as UserPayload);
+        }
+      });
+    });
+  };
+
+  static setAccessTokenCookie = async (res: Response, accessToken: string) => {
+    res.cookie('access_token', accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict'
+    });
+  };
+}
